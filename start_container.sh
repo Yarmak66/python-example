57c30844a040d99d662e0b01dbe70e8226b9734a
Pavel Yarmalovich <83726898+Yarmak66@users.noreply.github.com>
2024-10-21 01:53:50 +0200

    add this file for code deploy

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull pavelyarmalovich/simple-python-flask-project:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 pavelyarmalovich/simple-python-flask-project:latest
